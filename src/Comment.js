39a35a61d2917bc467973e0dc3a5247419c1cb89
ajin3830 <ajin3830@gmail.com>
2023-01-31 23:16:24 -0500

    code along

--- src/Comment.js
+++ src/Comment.js
@@ -3,5 +3,16 @@ import React from "react";
 function Comment() {
   return <div>Naturally, I agree with this article.</div>;
 }
+// Either the function component above
+// or the arrow function syntax below 
+// would give you the same outcome
+// const Comment = () => <div>Naturally, I agree with this article.</div>
+
+// class component like below works the same but it's legacy now
+// class Comment extends React.Component {
+//   render() {
+//     return<div>Naturally, I agree with this article.</div>
+//   }
+// }
 
 export default Comment;
